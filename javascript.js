39bd3a6f3821be6af78943feb993f6a0c746e91e
Megan Lynch <megantherese314@gmail.com>
2025-07-14 15:20:32 -0400

    Create 16x16 grids

---
+++ javascript.js
@@ -0,0 +1,21 @@
+
+
+const container = document.querySelector("#container");
+
+// create a 16x16 square of divs
+
+for (let i=1; i<=16; i++ ){
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j=1; j<=16; j++ ){
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        row.appendChild(grid);
+    }
+
+    container.appendChild(row);
+
+}
+
+
