65e16ec082893ebd6fee930f1feea46cccffcf5a
Megan Lynch <megantherese314@gmail.com>
2025-07-16 12:54:39 -0400

    Add button with dialogue

--- javascript.js
+++ javascript.js
@@ -1,29 +1,66 @@
 
+const button = document.querySelector("#restart_button");
+createGrid(16);
 
-const container = document.querySelector("#container");
 
-// create a 16x16 square of divs
+button.addEventListener("click", () => {
+  const input = prompt("Enter the number of squares:");
+  const totalSquares = parseInt(input);
 
-for (let i=1; i<=16; i++ ){
-    const row = document.createElement("div");
-    row.classList.add("row");
+  if (totalSquares > 100) {
+    alert("Please enter a number under 100.");
+    return;
+  }
+  removeGrid();
+  createGrid(totalSquares);
+});
 
-    for (let j=1; j<=16; j++ ){
-        const grid = document.createElement("div");
-        grid.classList.add("grid");
-        row.appendChild(grid);
-    }
-    container.appendChild(row);
+
+
+function createGrid(size) {
+
+  const container = document.querySelector("#container");
+  
+  // create a 16x16 square of divs
+  const widthHeight = 800 / size + "px";
+
+  for (let i=1; i<=size; i++ ){
+      const row = document.createElement("div");
+      row.classList.add("row");
+      for (let j=1; j<=size; j++ ){
+          const grid = document.createElement("div");
+          grid.classList.add("grid");
+          row.appendChild(grid);
+          grid.style.width = widthHeight;
+          grid.style.height = widthHeight;
+      }
+      container.appendChild(row);
+  }
+
+  const myGrid = document.querySelectorAll('.grid');
+
+  myGrid.forEach((square) => {
+    // and for each one we add a 'click' listener
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = 'lightblue';
+    });
+  });
+
+}
+
+function removeGrid(){
+  const myRows = document.querySelectorAll('.row');
+  const myGrid = document.querySelectorAll('.grid');
+  myGrid.forEach((grid) => {
+    if (myGrid) { 
+          grid.remove();
+      }});
+  myRows.forEach((row) => {
+    if (myRows) { 
+          row.remove();
+      }});
 
 }
 
 
-const myGrid = document.querySelectorAll('.grid');
-
-myGrid.forEach((square) => {
-  // and for each one we add a 'click' listener
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = 'lightblue';
-  });
-});
 
